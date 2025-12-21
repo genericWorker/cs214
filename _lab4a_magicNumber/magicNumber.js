500556f5fda0e2c20d345b6be4a30fa27aaa8905
genericWorker <generic.worker@gmail.com>
2025-12-21 17:14:42 -0500

    Rewrote logic to avoid document.write

--- _lab4a_magicNumber/magicNumber.js
+++ _lab4a_magicNumber/magicNumber.js
@@ -1,31 +1,48 @@
-                
+// 1. Setup the logic and get input
 var guess = prompt("Enter a whole number");
 var guessNumber = parseInt(guess);
 
-if (isNaN(guess)) {
+// Validate input
+if (isNaN(guessNumber)) {
     alert("Not a number!");
-    window.location.reload(false)
+    window.location.reload();
+} else {
+    var origNumber = guessNumber;
+    let outputHtml = "<ul>";
+
+    // Step 1: Add 9
+    let step1 = guessNumber + 9;
+    outputHtml += `<li>I added 9 to ${guessNumber}. The new number is ${step1}.</li>`;
+    guessNumber = step1;
+    
+    // Step 2: Multiply by 2
+    let step2 = guessNumber * 2;
+    outputHtml += `<li>I multiplied ${guessNumber} by 2. The new number is ${step2}.</li>`;
+    guessNumber = step2;
+    
+    // Step 3: Subtract 4
+    let step3 = guessNumber - 4;
+    outputHtml += `<li>I subtracted 4 from ${guessNumber}. The new number is ${step3}.</li>`;
+    guessNumber = step3;
+    
+    // Step 4: Divide by 2
+    let step4 = guessNumber / 2;
+    outputHtml += `<li>I divided ${guessNumber} by 2. The new number is ${step4}.</li>`;
+    guessNumber = step4;
+    
+    // Step 5: Subtract original
+    outputHtml += `<li>I subtracted your original number of ${origNumber} from ${guessNumber}.</li>`;
+    outputHtml += "</ul>";
+    
+    guessNumber -= origNumber;
+    
+    // Final Output
+    outputHtml += `<h3>Your final number is ${guessNumber}</h3>`;
+    // Note: Ensure "magicNumber.png" is in your project folder or the image won't show!
+    outputHtml += '<img src="magicNumber.png" alt="Magic Number Image" style="width:200px;">'; 
+    outputHtml += `<h1>Thanks for playing Magic Number.</h1>`;
+    outputHtml += `<button onclick="window.location.reload()">Play Again?</button>`;
+
+    // 3. Inject everything into the body at once
+    document.body.insertAdjacentHTML('beforeend', outputHtml);
 }
-
-var tempNumber = guessNumber;
-var origNumber = guessNumber;
-guessNumber += 9;
-document.write("<ul>");
-
-document.write("<li>I added  9 to " + tempNumber + ". The new number is " + guessNumber + "</li>");
-tempNumber = guessNumber;
-guessNumber *= 2;
-
-document.write("<li>I multiplied " + tempNumber + " by 2. " + "The new number is " + guessNumber + ".</li>");
-tempNumber = guessNumber;
-guessNumber -= 4;
-document.write("<li>I subtracted 4 from " + tempNumber + "." + "The new number is " + guessNumber + ".</li>");
-tempNumber = guessNumber;
-guessNumber = guessNumber / 2;
-document.write("<li>I divided " + tempNumber + " by 2." + "The new number is " + guessNumber + ".</li>");
-tempNumber = guessNumber;
-document.write("<li>I subtracted your original number of " + origNumber + " from " + guessNumber + ".</li></ul>");
-guessNumber -= origNumber;
-document.write("<h3>Your final number is " + guessNumber + "<h3>");
-document.write("<h1> Thanks for playing Magic Number.  ");
-document.write("<a href = \"javascript:window.location.reload(false)\"><br> Play Again? </a></h1>");
